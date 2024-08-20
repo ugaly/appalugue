87a49eb6678c2e3abe284b98823e55e097065a64
ugaly <113047155+ugaly@users.noreply.github.com>
2024-08-20 20:30:58 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 import Routes from './src/router';
@@ -11,18 +9,17 @@ export default function App() {
   let [fontsLoaded] = useFonts({
     Montserrat_400Regular,
     Montserrat_500Medium,
-    Montserrat_700Bold
+    Montserrat_700Bold,
   });
 
-  if(!fontsLoaded){
-    return <AppLoading/>
+  if (!fontsLoaded) {
+    return null; // Or you can return a custom loading component
   }
 
   return (
     <>
       <StatusBar style="light" backgroundColor="#000" translucent={false} />
-      <Routes/>
+      <Routes />
     </>
   );
 }
-
